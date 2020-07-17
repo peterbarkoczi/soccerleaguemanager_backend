5e4498978086eca3cd01a5dfac054c366a5364f2
peterbarkoczi <barkopetya@gmail.com>
2020-07-17 15:28:42 +0200

    add liga/list route

--- src/main/java/com/barkoczi/peter/soccerleaguemanager/controller/LeagueController.java
+++ src/main/java/com/barkoczi/peter/soccerleaguemanager/controller/LeagueController.java
@@ -1,4 +1,25 @@
 package com.barkoczi.peter.soccerleaguemanager.controller;
 
+import com.barkoczi.peter.soccerleaguemanager.entity.Leagues;
+import com.barkoczi.peter.soccerleaguemanager.repository.LeagueRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
 public class LeagueController {
+
+    @Autowired
+    private LeagueRepository leagueRepository;
+
+    @GetMapping("/liga/list")
+    public List<Leagues> leagueList() {
+        List<Leagues> leagues = leagueRepository.findAllLeague();
+        System.out.println(leagues);
+        return leagues;
+    }
 }
