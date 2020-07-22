b371f42364c1ac73547fcee38d5b483c24fa2781
peterbarkoczi <barkopetya@gmail.com>
2020-07-22 16:19:05 +0200

    rename Teams.java to Team.java and add league and location columns

---
+++ src/main/java/com/barkoczi/peter/soccerleaguemanager/entity/Team.java
@@ -0,0 +1,37 @@
+package com.barkoczi.peter.soccerleaguemanager.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "team")
+public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @JsonIgnore
+    @ManyToMany(cascade = CascadeType.PERSIST)
+    private List<League> league = new ArrayList<>();
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @JsonIgnore
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    private Location location;
+
+}
