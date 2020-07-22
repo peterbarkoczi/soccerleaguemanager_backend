ab2994e24b408f673cde22501564a371ed7f8514
peterbarkoczi <barkopetya@gmail.com>
2020-07-22 16:25:26 +0200

    created LocationService for add new location process

---
+++ src/main/java/com/barkoczi/peter/soccerleaguemanager/service/LocationService.java
@@ -0,0 +1,36 @@
+package com.barkoczi.peter.soccerleaguemanager.service;
+
+import com.barkoczi.peter.soccerleaguemanager.entity.League;
+import com.barkoczi.peter.soccerleaguemanager.entity.Location;
+import com.barkoczi.peter.soccerleaguemanager.repository.LocationRepository;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Service
+public class LocationService {
+
+    @Autowired
+    LocationRepository locationRepository;
+
+    public Location addNewLocation(Location location) {
+        Location newLocation = Location.builder()
+                .name(location.getName())
+                .leagues(new ArrayList<>())
+                .teams(new HashSet<>())
+                .build();
+
+        locationRepository.saveAndFlush(newLocation);
+
+        return newLocation;
+    }
+
+}
