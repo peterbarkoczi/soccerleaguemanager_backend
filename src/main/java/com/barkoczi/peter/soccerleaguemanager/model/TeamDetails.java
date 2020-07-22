cf98803250851f1745054b2821867a4fe22abe6a
peterbarkoczi <barkopetya@gmail.com>
2020-07-22 16:27:27 +0200

    created TeamDetails component

---
+++ src/main/java/com/barkoczi/peter/soccerleaguemanager/model/TeamDetails.java
@@ -0,0 +1,20 @@
+package com.barkoczi.peter.soccerleaguemanager.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Service
+public class TeamDetails {
+
+    private String teamName;
+    private Long locationId;
+    private String leagueName;
+
+}
