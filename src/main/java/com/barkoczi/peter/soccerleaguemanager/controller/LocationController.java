42bb68a3e85d451b6313c8d4e65de92f18b73d83
peterbarkoczi <barkopetya@gmail.com>
2020-07-22 16:23:13 +0200

    create new PostMapping for add new location

---
+++ src/main/java/com/barkoczi/peter/soccerleaguemanager/controller/LocationController.java
@@ -0,0 +1,30 @@
+package com.barkoczi.peter.soccerleaguemanager.controller;
+
+import com.barkoczi.peter.soccerleaguemanager.entity.Location;
+import com.barkoczi.peter.soccerleaguemanager.repository.LocationRepository;
+import com.barkoczi.peter.soccerleaguemanager.service.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+public class LocationController {
+
+    @Autowired
+    private LocationRepository locationRepository;
+
+    @Autowired
+    private LocationService locationService;
+
+    @GetMapping("/liga/list")
+    public List<Location> leagueList() {
+        return locationRepository.findAll();
+    }
+
+    @PostMapping("/location/add_location")
+    public Location addNewLocation(@RequestBody Location location) {
+        return locationService.addNewLocation(location);
+    }
+}
