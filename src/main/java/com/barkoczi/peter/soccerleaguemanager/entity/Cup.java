3f1f3e0c4f556fd3d7255c24de44088e905bbeb7
peterbarkoczi <barkopetya@gmail.com>
2020-07-23 23:35:57 +0200

    created Cup entity

---
+++ src/main/java/com/barkoczi/peter/soccerleaguemanager/entity/Cup.java
@@ -0,0 +1,36 @@
+package com.barkoczi.peter.soccerleaguemanager.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "cup")
+public class Cup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private String date;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @JsonIgnore
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    private Location location;
+
+    @OneToMany(cascade = CascadeType.PERSIST, mappedBy = "cup")
+    private List<Match> matches = new ArrayList<>();
+
+}
