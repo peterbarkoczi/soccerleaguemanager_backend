a46f8d036d4814b1b1bb5bb5e5fdc7c5a9a160d2
peterbarkoczi <barkopetya@gmail.com>
2020-07-23 10:58:17 +0200

    refactor league repository name

--- src/main/java/com/barkoczi/peter/soccerleaguemanager/repository/LeagueRepository.java
+++ src/main/java/com/barkoczi/peter/soccerleaguemanager/repository/LeagueRepository.java
@@ -3,7 +3,7 @@ package com.barkoczi.peter.soccerleaguemanager.repository;
 import com.barkoczi.peter.soccerleaguemanager.entity.League;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LeaguesRepository extends JpaRepository<League, Long> {
+public interface LeagueRepository extends JpaRepository<League, Long> {
 
     League findFirstByName(String name);
 
