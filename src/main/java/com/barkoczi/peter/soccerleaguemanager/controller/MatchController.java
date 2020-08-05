310c1b3e748356b7fd789811254374e280787b61
peterbarkoczi <barkopetya@gmail.com>
2020-08-05 10:00:10 +0200

    created get_qualifier, get_semifinals, create_semi_finals and update finished endpoints

--- src/main/java/com/barkoczi/peter/soccerleaguemanager/controller/MatchController.java
+++ src/main/java/com/barkoczi/peter/soccerleaguemanager/controller/MatchController.java
@@ -1,12 +1,12 @@
 package com.barkoczi.peter.soccerleaguemanager.controller;
 
 import com.barkoczi.peter.soccerleaguemanager.entity.Match;
+import com.barkoczi.peter.soccerleaguemanager.repository.MatchRepository;
 import com.barkoczi.peter.soccerleaguemanager.service.MatchService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController
@@ -15,8 +15,32 @@ public class MatchController {
     @Autowired
     private MatchService matchService;
 
+    @Autowired
+    private MatchRepository matchRepository;
+
+    @GetMapping("/match/get_qualifiers")
+    public List<Match> getQualifiers(@RequestParam Long cupId, String matchType) {
+        return matchRepository.findAllByCupIdAndMatchType(cupId, matchType);
+    }
+
+    @GetMapping("/match/get_semifinals")
+    public List<Match> getSemiFinals(@RequestParam Long cupId, String matchType) {
+        return matchRepository.findAllByCupIdAndMatchType(cupId, matchType);
+    }
+
+    @GetMapping("/match/create_semi_finals")
+    public List<Match> createSemiFinals(@RequestParam Long cupId, String matchType) {
+        return matchService.createSemiFinals(cupId, matchType);
+    }
+
     @PostMapping("/match/update_score")
     public void updateScore(@RequestBody Match match) {
         matchService.updateScore(match);
     }
+
+    @PostMapping("match/update_finished")
+    public void updateFinished(@RequestBody Match match) {
+        matchService.setFinished(match);
+    }
+
 }
