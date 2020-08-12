4b2a6c385e7fc28b15189f2d85f5b0815db17a55
peterbarkoczi <barkopetya@gmail.com>
2020-08-12 15:14:34 +0200

    created CardDetails model

---
+++ src/main/java/com/barkoczi/peter/soccerleaguemanager/model/CardDetails.java
@@ -0,0 +1,21 @@
+package com.barkoczi.peter.soccerleaguemanager.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Service
+public class CardDetails {
+
+    Long id;
+    String card1;
+    String card2;
+    String type;
+
+}
