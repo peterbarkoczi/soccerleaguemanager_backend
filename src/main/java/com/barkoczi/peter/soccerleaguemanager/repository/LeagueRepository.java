b0fb0f6639d4d1b66b2597bd63119b2ea1be7fcb
peterbarkoczi <barkopetya@gmail.com>
2020-08-26 15:28:21 +0200

    created  query method to find leagues by location id

--- src/main/java/com/barkoczi/peter/soccerleaguemanager/repository/LeagueRepository.java
+++ src/main/java/com/barkoczi/peter/soccerleaguemanager/repository/LeagueRepository.java
@@ -3,8 +3,14 @@ package com.barkoczi.peter.soccerleaguemanager.repository;
 import com.barkoczi.peter.soccerleaguemanager.entity.League;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LeagueRepository extends JpaRepository<League, Long> {
 
     League findFirstByName(String name);
+
+    League findFirstById(Long id);
+
+    List<League> findLeaguesByLocation_IdOrderByIdAsc(Long locationId);
 
 }
