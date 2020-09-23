ccb8619e0126fa4ed15921cf2df569a7f66e216d
peterbarkoczi <barkopetya@gmail.com>
2020-09-23 14:29:04 +0200

    added new columns to TeamStat entity

--- src/main/java/com/barkoczi/peter/soccerleaguemanager/model/TeamStat.java
+++ src/main/java/com/barkoczi/peter/soccerleaguemanager/model/TeamStat.java
@@ -18,6 +18,9 @@ public class TeamStat {
     private int receivedScore = 0;
     private int difference = 0;
     private int playedMatch = 0;
+    private int win = 0;
+    private int lose = 0;
+    private int draw = 0;
     private String team;
 
 }
