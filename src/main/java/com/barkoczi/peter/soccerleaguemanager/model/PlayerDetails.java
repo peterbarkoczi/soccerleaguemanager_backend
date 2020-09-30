f34d68256b1cf6e595405a371bbb04603539cd22
peterbarkoczi <barkopetya@gmail.com>
2020-10-01 00:12:03 +0200

    created player details class

---
+++ src/main/java/com/barkoczi/peter/soccerleaguemanager/model/PlayerDetails.java
@@ -0,0 +1,27 @@
+package com.barkoczi.peter.soccerleaguemanager.model;
+
+import com.barkoczi.peter.soccerleaguemanager.entity.Cup;
+import com.barkoczi.peter.soccerleaguemanager.entity.League;
+import com.barkoczi.peter.soccerleaguemanager.entity.Player;
+import com.barkoczi.peter.soccerleaguemanager.entity.Team;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Service
+public class PlayerDetails {
+
+    private Player player;
+    private Set<Team> teams;
+    private Set<League> leagues;
+    private Set<Cup> cups;
+
+}
