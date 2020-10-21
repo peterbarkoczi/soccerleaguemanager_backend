1b1b9b44eeae16774b705f7d8f9bb51066416ea8
peterbarkoczi <barkopetya@gmail.com>
2020-10-21 21:30:12 +0200

    refactor

--- src/main/java/com/barkoczi/peter/soccerleaguemanager/controller/UserinfoController.java
+++ src/main/java/com/barkoczi/peter/soccerleaguemanager/controller/UserinfoController.java
@@ -3,9 +3,11 @@ package com.barkoczi.peter.soccerleaguemanager.controller;
 import com.barkoczi.peter.soccerleaguemanager.entity.AppUser;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController()
 public class UserinfoController {
 
@@ -13,7 +15,7 @@ public class UserinfoController {
     public String currentUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         AppUser user = (AppUser) authentication.getPrincipal();
-        return user.getUsername() + "\n" + user.getRoles();
+        return user.getUsername() + "\n" + user.getRole();
     }
 
 }
--- src/main/java/com/barkoczi/peter/soccerleaguemanager/model/user/SigninCredentials.java
+++ src/main/java/com/barkoczi/peter/soccerleaguemanager/model/user/SigninCredentials.java
@@ -1,4 +1,4 @@
-package com.barkoczi.peter.soccerleaguemanager.model;
+package com.barkoczi.peter.soccerleaguemanager.model.user;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserCredentials {
+public class SigninCredentials {
 
     private String username;
     private String password;
